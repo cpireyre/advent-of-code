51ec3fb97ee73fa967ed8a66e0235bd190a856c6
Colin <cpireyre@student.42.fr>
2025-10-05 10:16:47 +0300

    2015: 01

---
+++ 2015/day01.py
@@ -0,0 +1,17 @@
+def I(xs):
+    res = 0
+    for c in xs:
+        res += c == '('
+        res -= c == ')'
+    return res
+
+def II(xs):
+    res = 0
+    for i, c in enumerate(xs):
+        res += c == '('
+        res -= c == ')'
+        if (res == -1): return i + 1
+
+input = open("input").read()
+print(I(input))
+print(II(input))
