bbd75abf2bb25a418dd9f4b27c97d6a3c70ae27b
Colin <cpireyre@student.42.fr>
2025-10-10 20:33:36 +0300

    itertools dark arts

--- 2018/day01.py
+++ 2018/day01.py
@@ -1,23 +1,22 @@
 from sys import argv
+from itertools import cycle
 input = open("input/" + argv[0] + ".txt").read().split("\n")
 
-def I(lines):
-    freq = 0
-    for L in lines:
-        if L[0] == "-": freq -= int(L[1:])
-        if L[0] == "+": freq += int(L[1:])
-    return freq
+def freq(lines):
+    f = 0
+    for delta in cycle(map(int, lines)):
+        yield (f := f + delta)
+
+def I(input):
+    xs = freq(input)
+    for _ in range(len(input)): res = next(xs)
+    return res
+
+def II(input):
+    seen = {0}
+    for x in freq(input):
+        if x in seen: return x
+        seen.add(x)
 
 print(I(input))
-
-def II(lines):
-    freq = 0
-    seen = set()
-    while True:
-        for L in lines:
-            if L[0] == "-": freq -= int(L[1:])
-            if L[0] == "+": freq += int(L[1:])
-            if freq in seen: return freq
-            seen.add(freq)
-
 print(II(input))
