d1da30ecd08eac822e5c038452bea60a8ebfbcaa
Colin <cpireyre@student.42.fr>
2025-12-01 11:45:53 +0200

    Consolidate

--- 2025/01.py
+++ 2025/01.py
@@ -1,27 +1,20 @@
-example = open("./input/01-ex.txt").read().split("\n")
-input   = open("./input/01.txt")   .read().split("\n")
+example = open("./input/01-ex.txt").read().split()
+input   = open("./input/01.txt")   .read().split()
 
-def I(lines):
-    dial, res = 50, 0
-    sign = {"L": -1, "R": 1}
-    for turn in lines:
-        dial += sign[turn[0]] * int(turn[1:])
-        dial %= 100
-        res += dial == 0
-    return res
-
-def II(lines):
-    dial, res = 50, 0
+def f(lines):
+    p1 = p2 = 0
+    dial = 50
     sign = {"L": -1, "R": 1}
     for turn in lines:
         inc = int(turn[1:])
         for _ in range(inc):
             dial += sign[turn[0]]
             dial %= 100
-            res += dial == 0
-    return res
+            p2 += dial == 0
+        p1 += dial == 0
+    return (p1, p2)
 
-print(I(example)) # 3
-print(II(example)) # 6
-print(I(input)) # 1064
-print(II(input)) # 6122
+assert f(example)[0] == 3 # 3
+assert f(example)[1] == 6 # 6
+assert f(input)[0] == 1064 # 1064
+assert f(input)[1] == 6122 # 6122
