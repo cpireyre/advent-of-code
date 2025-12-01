3496ff46d58bfa0b86eeebc0944466de9de23699
Colin <cpireyre@student.42.fr>
2025-12-01 13:41:41 +0200

    Punchcard-sized

--- 2025/01.py
+++ 2025/01.py
@@ -1,20 +1,11 @@
-example = open("./input/01-ex.txt").read().split()
-input   = open("./input/01.txt")   .read().split()
-
-def f(lines):
-    p1 = p2 = 0
-    dial = 50
-    sign = {"L": -1, "R": 1}
-    for turn in lines:
-        inc = int(turn[1:])
-        for _ in range(inc):
-            dial += sign[turn[0]]
-            dial %= 100
-            p2 += dial == 0
-        p1 += dial == 0
-    return (p1, p2)
-
-assert f(example)[0] == 3 # 3
-assert f(example)[1] == 6 # 6
-assert f(input)[0] == 1064 # 1064
-assert f(input)[1] == 6122 # 6122
+p1 = p2 = 0
+dial = 50
+sign = {"L": -1, "R": 1}
+for turn in open(0).read().split():
+    inc = int(turn[1:])
+    for _ in range(inc):
+        dial += sign[turn[0]]
+        dial %= 100
+        p2 += dial == 0
+    p1 += dial == 0
+print(p1, p2)
