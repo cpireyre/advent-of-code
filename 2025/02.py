8cd7effd054b4a11b46350ec3257317e77c6a2f1
Colin Pireyre <copireyr@student.hive.fi>
2025-12-02 14:03:19 +0200

    Double punchcard but slow

--- 2025/02.py
+++ 2025/02.py
@@ -1,28 +1,19 @@
 from math import pow, log10
-
-shift = lambda n,r: n // int(pow(10, r))
-maskoff = lambda n,r: n % int(pow(10, r))
-n_digits = lambda n: int(log10(n)) + 1
-def chunked(n, i):
-    res = []
-    while n:
-        m = maskoff(n, i)
-        if (m and n_digits(m) != i):
-            return None
-        res.append(m)
-        n = shift(n, i)
-    return res
-
-input = open("./input/02-ex.txt").read().split(",")
+ilog10   = lambda n:   int(log10(n))
+ipow     = lambda n,i: int(pow(n,i))
+n_digits = lambda n:   ilog10(n) + 1
 p1 = p2 = 0
-for id_range in input:
-    nums = id_range.split("-")
-    l, r = int(nums[0]), int(nums[1])
-    for n in range(l, r+1):
+for ids in open(0).read().split(","):
+    l, r = ids.split("-")
+    for n in range(int(l), int(r)+1):
+        m = pow(10, log10(n)//2 + 1)
+        p1 += ((n//m)*(m+1)==n)*n*(ilog10(n)%2)
         for i in reversed(range(1, n_digits(n))):
-            xs = chunked(n, i)
-            if not xs: continue
-            if all(x == xs[0] for x in xs):
-                p2 += n
-                break
+            q = n
+            while q:
+                m = q % ipow(10, i)
+                if (m and n_digits(m) != i) or m != n % ipow(10, i):
+                    break
+                q = q // ipow(10, i)
+            if not q: p2 += n; break
 print(p1, p2)
