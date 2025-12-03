576acd905dfdbd8c57b675fbade15f33f670150f
Colin <cpireyre@student.42.fr>
2025-12-03 11:54:05 +0200

    Punchcard-sized with memoization

--- 2025/03.py
+++ 2025/03.py
@@ -1,13 +1,12 @@
-from itertools import combinations
-from functools import reduce
-input = open("./input/03-ex.txt").read().splitlines()
-S = lambda xs: reduce(lambda a,b: 10*a+b, xs, 0)
+from functools import cache
+@cache
+def f(xs, n):
+    if not n:        return 0
+    if len(xs) == n: return int(xs)
+    x = int(xs[0])
+    a = 10**(n-1) * x + f(xs[1:], n-1)
+    return max(a, f(xs[1:], n))
 p1 = p2 = 0
-for line in input:
-    L = combinations(map(int, line), 2)
-    M = combinations(map(int, line), 12)
-    t = max(L, key=S)
-    u = max(M, key=S)
-    p1 += S(t)
-    p2 += S(u)
-print(p1, p2)
+for xs in open(0).read().splitlines():
+    p1 += f(xs, 2); p2 += f(xs, 12)
+print(p1,p2)
