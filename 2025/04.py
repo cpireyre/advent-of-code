375d7b327d8bac98fd0249555d3306c0faa59051
Colin <cpireyre@student.42.fr>
2025-12-04 09:26:56 +0200

    Use sets

--- 2025/04.py
+++ 2025/04.py
@@ -1,20 +1,21 @@
 from itertools import product
-grid = list(map(list, open("./input/04-in.txt").read().split()))
+
+grid = open("./input/04-in.txt").read().splitlines()
 R, C = len(grid), len(grid[0])
+
 inside = lambda r,c: 0 <= r < R and 0 <= c < C
-paper = lambda r,c: grid[r][c] == '@'
-neighbors = lambda r,c: [(r+1,c),(r-1,c),(r,c+1),(r,c-1),
-                          (r+1,c+1),(r-1,c-1),(r+1,c-1),(r-1,c+1)]
+neighbors = lambda r,c: {(r+1,c),(r-1,c),(r,c+1),(r,c-1),
+                          (r+1,c+1),(r-1,c-1),(r+1,c-1),(r-1,c+1)}
+adjacent = lambda u: sum(inside(*v) and v in paper for v in neighbors(*u))
 p1 = p2 = 0
-prev = 0
+paper = set()
+for r,c in product(range(R),range(C)):
+    if grid[r][c] == '@': paper.add((r,c))
 while True:
-    to_remove = set()
-    for u in product(range(R),range(C)):
-        if paper(*u):
-            remove = sum(inside(*v) and paper(*v) for v in neighbors(*u)) < 4
-            if remove: to_remove.add(u)
-    for r,c in to_remove: grid[r][c] = '.'
-    if not p2: p1 += len(to_remove)
-    p2 += len(to_remove)
-    if prev == len(to_remove): break
+    keep = set(u for u in paper if adjacent(u) >= 4)
+    if not p2: p1 += len(paper) - len(keep)
+    p2 += len(paper) - len(keep)
+    if paper == keep: break
+    paper = keep
+
 print(p1, p2)
