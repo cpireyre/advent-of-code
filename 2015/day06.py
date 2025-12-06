a722d9fe1b7b49e55b39768231dc34725c0562de
Colin <cpireyre@student.42.fr>
2025-12-06 15:00:58 +0200

    Rm

--- 2015/day06.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-xs = open("./input/" + argv[0] + ".txt").read().split("\n")
-for instruction in xs:
-    if   instruction[0:8] == "turn off": print("off")
-    elif instruction[0:6] == "toggle": print("toggle")
-    elif instruction[0:7] == "turn on": print("on")
-    else: print(instruction)
-    print(instruction)
