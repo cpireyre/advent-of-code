7f9aea6b867d89b34c6246c57d198aff3159eeec
Colin <cpireyre@student.42.fr>
2025-12-06 14:54:15 +0200

    Slightly clearer set arithmetic

--- 2025/04.py
+++ 2025/04.py
@@ -6,6 +6,6 @@ adj = lambda u: sum(G[r][c]=='@'
 while True:
     K = set(u for u in P if adj(u) >= 4)
     if P == K: break
-    p1 += len(P^K)*(not p2);p2+=len(P^K)
+    p1 += len(P-K)*(not p2);p2+=len(P-K)
     P = K
 print(p1, p2)
