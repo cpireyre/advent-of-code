a41678bdada14ab1b7bb3d8b0788fef18aba8dc0
Colin <cpireyre@student.42.fr>
2025-12-07 18:33:03 +0200

    2025-12-07 Not filtered yet

--- 2025/07.py
+++ 2025/07.py
@@ -1,4 +1,25 @@
-from utils import extract
-G = open("./input/07-ex.txt").read().split()
-splitters = extract(G, lambda u: u == '^')
-print(splitters)
+G = open("./input/07-in.txt").read().split()
+start = G[0].index('S')
+beams = {start}
+p1 = 0
+for line in G[::2]:
+    splits = set()
+    for beam in beams:
+        if line[beam] == '^':
+            splits.add(beam - 1)
+            splits.add(beam + 1)
+            p1 += 1
+        else:
+            splits.add(beam)
+        beams = splits
+print(p1)
+
+from functools import cache
+R = len(G)
+@cache
+def timelines(r,c):
+    while G[r][c] != '^':
+        r += 2
+        if r >= R: return 1
+    return timelines(r,c-1) + timelines(r,c+1)
+print(timelines(0,start))
