d9c746dddd93a4b472168af6a879816fa51088e8
Colin <cpireyre@student.42.fr>
2025-12-09 17:23:39 +0200

    ok fine

--- 2025/09.py
+++ 2025/09.py
@@ -1,31 +1,20 @@
-from itertools import combinations
-from functools import cache
-import heapq
-data = open("./input/09-ex.txt").read().splitlines()
-S = set()
-R = C = 0
-for l in data:
-    c,r = map(int, l.split(','))
-    S.add((r,c))
-    R = max(R, r); C = max(C, c)
-E, X, Y = [], [], []
-for (r,c),(x,y) in combinations(S, 2):
-    H, W = abs(r - x) + 1, abs(c - y) + 1
-    heapq.heappush(E, (-H*W, (r,c),(x,y)))
-    X.append((r,x)); Y.append((c,y))
-print(-E[0][0])
-print(X);print(Y)
+# cf. r/adventofcode/comments/1phywvn/comment/nt2hps9/
 
-L = len(X)
-assert L == len(Y)
-@cache
-def inside(px,py):
-    crossings = 0
-    for i in range(L):
-        (x1,x2),(y1,y2) = X[i],Y[i]
-        x1 -= px; x2 -= px; y1 -= py; y2 -= py
-        if y1 * y2 < 0:
-            if x1 >= 0 and x2 >= 0: crossings += 1
-            else:
-                ???
-    return crossings % 2 == 1
+from itertools import pairwise, combinations
+data = open("./input/09-in.txt").read().splitlines()
+vertices = list(map(eval, data))
+edges = list(pairwise(vertices + [vertices[0]]))
+p1 = p2 = 0
+for (u,v),(x,y) in combinations(vertices, 2):
+    area = (abs(x-u)+1) * (abs(y-v)+1)
+    p1 = max(p1, area)
+    if area > p2:
+        for (p,q), (r,s) in edges:
+            if not (min(u,x) >= max(p,r)   # right?
+                 or max(u,x) <= min(p,r)   # left?
+                 or max(v,y) <= min(q,s)   # above?
+                 or min(v,y) >= max(q,s)): # below?
+                break
+        else: p2 = area
+
+print(p1, p2)
