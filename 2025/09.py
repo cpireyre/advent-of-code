75f9d72e02d2576b2eaec1f7894128212b7c74b2
Colin <cpireyre@student.42.fr>
2025-12-09 12:30:14 +0200

    Brute force with heap

--- 2025/09.py
+++ 2025/09.py
@@ -1,14 +1,14 @@
-data = open("./input/09-ex.txt").read().splitlines()
+from itertools import combinations
+import heapq
+data = open("./input/09-in.txt").read().splitlines()
 S = set()
 R = C = 0
 for l in data:
     c,r = map(int, l.split(','))
     S.add((r,c))
     R = max(R, r); C = max(C, c)
-R += 1; C += 1
-G = [['.' for _ in range(C)] for _ in range(R)]
-for r,c in S: G[r][c] = '#'
-for r in range(R):
-    for c in range(C):
-        print(G[r][c], end=' ')
-    print('')
+E = []
+for (r,c),(x,y) in combinations(S, 2):
+    H, W = abs(r - x) + 1, abs(c - y) + 1
+    heapq.heappush(E, (-H*W, (r,c),(x,y)))
+print(-E[0][0])
