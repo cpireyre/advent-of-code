e43ae09b8dd267c2bce349d4172b00db06002f1d
Colin <cpireyre@student.42.fr>
2025-12-09 08:55:14 +0200

    Turns out I am inured to row-major

--- 2025/09.py
+++ 2025/09.py
@@ -1,10 +1,14 @@
-data = open("./input/09-in.txt").read().splitlines()
-S = [tuple(map(int, l.split(','))) for l in data]
-print(S)
-N, p1 = len(S), 0
-for r in range(N):
-    for c in range(r+1,N):
-        H = abs(S[r][0] - S[c][0]) + 1
-        W = abs(S[r][1] - S[c][1]) + 1
-        if H*W > p1: p1 = H*W
-print(p1)
+data = open("./input/09-ex.txt").read().splitlines()
+S = set()
+R = C = 0
+for l in data:
+    c,r = map(int, l.split(','))
+    S.add((r,c))
+    R = max(R, r); C = max(C, c)
+R += 1; C += 1
+G = [['.' for _ in range(C)] for _ in range(R)]
+for r,c in S: G[r][c] = '#'
+for r in range(R):
+    for c in range(C):
+        print(G[r][c], end=' ')
+    print('')
