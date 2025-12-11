2c6e3d48c9c23ee9c6226d3997c3cc49a6e09713
Colin <cpireyre@student.42.fr>
2025-12-11 07:54:41 +0100

    Memoization rather

--- 2025/11.py
+++ 2025/11.py
@@ -1,15 +1,29 @@
 from collections import defaultdict, deque
-data = open("./input/11-in.txt").read().splitlines()
+from functools import cache
+data = open("./input/11-ex.txt").read().splitlines()
 G = defaultdict(set)
 for line in data:
     source, *sinks = line.split()
     source = source[:-1]
     G[source].update(sinks)
-Q = deque(G["you"])
-p1 = 0
-while Q:
-    u = Q.popleft()
-    for v in G[u]:
-        if v != "out": Q.append(v)
-        else: p1 += 1
-print(p1)
+
+@cache
+def paths2(source, sink):
+    if source == sink: return 1
+    return sum(paths2(v, sink) for v in G[source])
+
+def paths(sources, sink):
+    Q = deque(sources)
+    res = 0
+    while Q:
+        u = Q.popleft()
+        for v in G[u]:
+            if v != sink: Q.append(v)
+            else: res += 1
+    return res
+
+# print(paths(G["you"], "out"))
+print(paths2("you", "out"))
+print(paths2("svr", "fft"))
+print(paths2("fft", "dac"))
+print(paths2("dac", "out"))
