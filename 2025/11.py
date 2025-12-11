1d3604e6c5f18a69b3b95c3f86c3ae918bc9bfb4
Colin <cpireyre@student.42.fr>
2025-12-11 08:00:10 +0100

    Little bit golf as a Christmas treat

--- 2025/11.py
+++ 2025/11.py
@@ -1,29 +1,15 @@
 from collections import defaultdict, deque
 from functools import cache
-data = open("./input/11-ex.txt").read().splitlines()
+data = open("./input/11-ex2.txt").read().splitlines()
 G = defaultdict(set)
-for line in data:
-    source, *sinks = line.split()
-    source = source[:-1]
-    G[source].update(sinks)
+for source, *sinks in map(str.split, data):
+    G[source[:-1]].update(sinks)
 
 @cache
-def paths2(source, sink):
-    if source == sink: return 1
-    return sum(paths2(v, sink) for v in G[source])
-
-def paths(sources, sink):
-    Q = deque(sources)
-    res = 0
-    while Q:
-        u = Q.popleft()
-        for v in G[u]:
-            if v != sink: Q.append(v)
-            else: res += 1
-    return res
+def paths(s,e):
+    if s == e: return 1
+    return sum(paths(v,e) for v in G[s])
 
 # print(paths(G["you"], "out"))
-print(paths2("you", "out"))
-print(paths2("svr", "fft"))
-print(paths2("fft", "dac"))
-print(paths2("dac", "out"))
+# print(paths("you", "out"))
+print(paths("svr", "out")) # 1
